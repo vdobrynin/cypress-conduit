18221b8981b27a5f08675946b4932c66d6cf1947
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-09-23 22:48:54 -0700

    api mocking #54

--- cypress/e2e/workingWithAPIs.cy.js
+++ cypress/e2e/workingWithAPIs.cy.js
@@ -1,5 +1,9 @@
 /// <reference types="cypress" />
 
 it('first test', () => {
+    // cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/tags', { fixture: 'tags.json' }) // #54 
+    // cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', { fixture: 'articles.json' }) // #54
+    cy.intercept('GET', '**/tags', { fixture: 'tags.json' })                 // wild card #54
+    cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })        // wild card #54
     cy.loginToApplication()
 })
