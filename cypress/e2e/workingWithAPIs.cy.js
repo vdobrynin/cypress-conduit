4a94244f44f18f82faa8a9202f83709b0bd14c7e
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-09-24 18:23:27 -0700

    router matcher #56

--- cypress/e2e/workingWithAPIs.cy.js
+++ cypress/e2e/workingWithAPIs.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress" />
 
-it('first test', () => {
+it.only('first test', () => {
+    cy.intercept({ method: 'GET', pathname: 'tags' }, { fixture: 'tags.json' })  //router matcher #56
     // cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/tags', { fixture: 'tags.json' }) // #54 
     // cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', { fixture: 'articles.json' }) // #54
-    cy.intercept('GET', '**/tags', { fixture: 'tags.json' })                 // wild card #54
+    // cy.intercept('GET', '**/tags', { fixture: 'tags.json' })                 // wild card #54
     cy.intercept('GET', '**/articles*', { fixture: 'articles.json' })        // wild card #54
     cy.loginToApplication()
 })
 
-it.only('modify api response', () => {                      // #55 mocking api response
+it('modify api response', () => {                      // #55 mocking api response
     cy.intercept('GET', '**/articles*', req => {
         req.continue(res => {
             res.body.articles[0].favoritesCount = 9999999
